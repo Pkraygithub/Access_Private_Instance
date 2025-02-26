f61975fe58507c36ca3e2eece9eca297c53f2a17
Pkraygithub <pankaj9576.kr@gmail.com>
2025-02-26 23:53:22 +0530

    First Commit

---
+++ Private_Instance/src/user/data/Test.java
@@ -0,0 +1,24 @@
+package user.data;
+
+import java.lang.reflect.Field;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception{
+		
+	//	User user= new User();
+		
+		Class<?> clz = Class.forName("user.data.User");
+		Field age = clz.getDeclaredField("age");
+		Field name = clz.getDeclaredField("name");
+		name.setAccessible(true);
+		age.setAccessible(true);
+		
+		Object obj = clz.newInstance();
+		User user= (User) obj;
+		name.set(user,"Pankaj Kumar");
+		age.set(user,30);
+		user.getUserAge();
+	}
+
+}
---
+++ Private_Instance/src/user/data/User.java
@@ -0,0 +1,14 @@
+package user.data;
+
+public class User {
+
+	private int age;
+
+	private String name;
+
+	public void getUserAge() {
+
+		System.out.println("Name::" + name + "\nAge ::" + age);
+	}
+
+}
