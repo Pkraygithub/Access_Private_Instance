d8f06b7e074337c1d526c0fc17f4a093ee0f8c90
Pkraygithub <pankaj9576.kr@gmail.com>
2025-02-27 00:03:20 +0530

    Second Commit

--- Private_Instance/src/user/data/Test.java
+++ Private_Instance/src/user/data/Test.java
@@ -4,20 +4,20 @@ import java.lang.reflect.Field;
 
 public class Test {
 
-	public static void main(String[] args) throws Exception{
-		
-	//	User user= new User();
-		
-		Class<?> clz = Class.forName("user.data.User");
+	public static void main(String[] args) throws Exception {
+
+		// User user= new User();
+
+		Class<?> clz = Class.forName("user.data.User"); // By Using Reflection Api We can Access Private Variable by outside of class
 		Field age = clz.getDeclaredField("age");
 		Field name = clz.getDeclaredField("name");
 		name.setAccessible(true);
 		age.setAccessible(true);
-		
+
 		Object obj = clz.newInstance();
-		User user= (User) obj;
-		name.set(user,"Pankaj Kumar");
-		age.set(user,30);
+		User user = (User) obj;
+		name.set(user, "Pankaj Kumar");
+		age.set(user, 30);
 		user.getUserAge();
 	}
 
